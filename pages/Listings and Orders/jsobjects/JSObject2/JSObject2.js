c963110262ce99ec7640bbc7eb9751bba721bdbf
Adam Bello <bello.ad@northeastern.edu>
2023-04-19 06:35:21 +0000

    added widgets/queries for listings and orders

---
+++ pages/Listings and Orders/jsobjects/JSObject2/JSObject2.js
@@ -0,0 +1,13 @@
+export default {
+	myVar1: [],
+	myVar2: {},
+	myFun1: () => {
+		//write code here
+		showModal('ListingDetailModal');
+		getListingDetails.run();
+		getListingReviews.run();
+	},
+	myFun2: async () => {
+		//use async-await or promises
+	}
+}
