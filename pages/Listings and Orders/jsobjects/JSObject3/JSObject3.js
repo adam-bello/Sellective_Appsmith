9e1a7579f9e9892aa64cbab408705494bcb0dcb2
Adam Bello <bello.ad@northeastern.edu>
2023-04-19 17:15:07 +0000

    fixed a couple buttons

---
+++ pages/Listings and Orders/jsobjects/JSObject3/JSObject3.js
@@ -0,0 +1,12 @@
+export default {
+	myVar1: [],
+	myVar2: {},
+	myFun1: () => {
+		//write code here
+	},
+	myFun2: async () => {
+		//use async-await or promises
+		await newListing.run();
+		await getListings.run();
+	}
+}
---
+++ pages/Listings and Orders/jsobjects/JSObject4/JSObject4.js
@@ -0,0 +1,17 @@
+export default {
+	myVar1: [],
+	myVar2: {},
+	myFun1: () => {
+		//write code here
+	},
+	myFun2: async () => {
+		//use async-await or promises
+		await editListing.run();
+		await getListings.run();
+	},
+	myFun3: async () => {
+		//use async-await or promises
+		await deleteListing.run();
+		await getListings.run();
+	}
+}
