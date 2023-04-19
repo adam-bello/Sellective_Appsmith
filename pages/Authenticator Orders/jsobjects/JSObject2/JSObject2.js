1ea95ad31df6580af51aa68952749f1a85d5bb1a
Justin Wu <jwu1723@gmail.com>
2023-04-19 21:42:39 +0000

    Authenticator Pages

---
+++ pages/Authenticator Orders/jsobjects/JSObject2/JSObject2.js
@@ -0,0 +1,12 @@
+export default {
+	myVar1: [],
+	myVar2: {},
+	myFun1: () => {
+		//write code here
+		showModal('orderDetailsModal');
+		getOrderDetails.run()
+	},
+	myFun2: async () => {
+		//use async-await or promises
+	}
+}
