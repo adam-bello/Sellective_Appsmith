d2d1909bb92a331f3203d6065545ed333e270471
Adam Bello <bello.ad@northeastern.edu>
2023-04-19 14:39:28 +0000

    added seller reviews/responses to new page

---
+++ pages/Seller Reviews/jsobjects/viewSellerReviewResponses/viewSellerReviewResponses.js
@@ -0,0 +1,12 @@
+export default {
+	myVar1: [],
+	myVar2: {},
+	myFun1: () => {
+		//write code here
+		showModal('viewReviewResponsesModal');
+		getSellerReviewResponses.run();
+	},
+	myFun2: async () => {
+		//use async-await or promises
+	}
+}
